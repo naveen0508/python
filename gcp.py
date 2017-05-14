ffaed7e556e963a1b7a1caa000b71c23a44499e0
Naveen Kumar <kumar.naveen0508@gmail.com>
2017-05-14 21:27:55 +0530

    Add files via upload

---
+++ gcp.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 14 12:00:53 2017
+
+@author: Naveen
+
+Greatest Common Factor program
+"""
+def gcp(m,n):
+    fm = []
+    for i in range(1, m+1):
+        if (m%i) == 0:
+            fm.append(i)
+    
+    fn = []
+    for j in range(1, n+1):
+        if (n%j) == 0:
+            fn.append(j)
+    
+    cf = []
+    for f in fm:
+        if f in fn:
+            cf.append(f)
+    
+    return(cf[-1])
+
+
+print(gcp(14,63))
